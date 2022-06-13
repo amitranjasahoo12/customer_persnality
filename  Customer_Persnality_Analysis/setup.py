a416797e7886b9259b883d91efa226771cb633bc
amitranjasahoo <amitranjasahoo@gmail.com>
2022-06-13 14:34:02 +0530

    first commit and added raw data

---
+++  Customer_Persnality_Analysis/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='What people say about your product: what gives customers’ attitude towards the',
+    author='Amit Ranjan Sahoo',
+    license='MIT',
+)
